cf11cdce880f9fa5301fce8298cfb31b1de6f922
AndyCheung-0v0 <AndyCheung-0v0@users.noreply.github.com>
2022-07-26 16:02:19 +0800

    Create 2DHeart.cpp
    A new program to show 2d heart

---
+++ _finished/MyTreasures/20220726_KeepItSecret/2DHeart.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath> 
+using namespace std; 
+
+int KeepItSecret() {
+
+     int Jayden=(0.0)      ,Hailey=(0.0);
+    cin >> Jayden; if(   Jayden == 520) for
+   (double lovely=1.36;-1.1<=lovely;lovely-=
+   0.06){for (double love= -1.25;1.2 >= love;
+   love+=0.025)cout<<((pow((love*love+lovely*
+     lovely-1.0),3)-love*love*lovely*/*  */
+      lovely*lovely<=0.0)?//Pr Heart Shap/
+        "LoveU*Hailey<3*"//By AKA Jayden/
+          [Jayden++%16]//wif C++ Build/
+            :' ');cout<<endl;}//Keep/
+              cin>>Jayden;/**It**/
+                return/*Secret*/
+                  520;/*@##@*/
+                   /*@*@*@*/
+                    /*##*/
+                     /**/
+                     //
+}
+
+int main() { KeepItSecret(); }
