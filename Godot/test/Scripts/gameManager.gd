25906ce18e4efc331dfdf21d929cf0e2d36641fe
JaydenCh_0v0 <57389495+JaydenCh-0v0@users.noreply.github.com>
2025-01-24 23:33:45 +0800

    new weeper and bird sprits

--- Godot/test/Scripts/gameManager.gd
+++ Godot/test/Scripts/gameManager.gd
@@ -5,10 +5,10 @@ extends Node2D
 @export var is_spawn_enemy: bool = true
 @export var score: int = 0
 @export var score_label: Label
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
